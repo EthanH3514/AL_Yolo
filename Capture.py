d7c38ccdfca9eff38447bb7506646677da70ca63
EthanH3514 <1326414961@qq.com>
2023-06-18 15:27:40 +0800

    更新截图方式

---
+++ Capture.py
@@ -0,0 +1,58 @@
+import dxcam
+import pyautogui
+from utils.augmentations import (Albumentations, augment_hsv, classify_albumentations, classify_transforms, copy_paste,
+                                 letterbox, mixup, random_perspective)
+import numpy as np
+import time
+
+WIDTH, HEIGHT = pyautogui.size()
+CENTER = [WIDTH/2, HEIGHT/2]
+FOV = 110
+DPI = 800
+MOUSE_SENSITIVITY = 5
+SIZE = 640
+SMOOTH = 1.2
+LEFT = int(CENTER[0] - SIZE / 2)
+TOP = int(CENTER[1] - SIZE / 2)
+REGION = (LEFT, TOP, LEFT+SIZE, TOP+SIZE)
+
+class LoadScreen:
+    def __init__(self, img_size = SIZE, stride = 32, auto=True, transforms=None, region: tuple[int,int,int,int]=REGION):
+        self.screen = 0
+        self.img_size = img_size
+        self.region = region
+        self.camera = dxcam.create(output_color="BGR")
+        self.frame = 0
+        self.stride = stride
+        self.auto = auto
+        self.transforms = transforms
+        self.left = region[0]
+        self.top = region[1]
+        self.width = img_size
+        self.height = img_size
+        
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        now_time = time.time()
+        
+        im0 = np.array(self.camera.grab(region=self.region))
+        while im0.any() == None:
+            im0 = np.array(self.camera.grab(region=self.region))
+        
+        s = f'screen {self.screen} (LTWH): {self.left},{self.top},{self.width},{self.height}: '
+        if self.transforms:
+            im = self.transforms(im0)
+        else:
+            im = letterbox(im0, self.img_size, stride=self.stride, auto=self.auto)[0]
+            im = im.transpose((2, 0, 1))[::-1]
+            im = np.ascontiguousarray(im)
+        self.frame += 1
+        
+        that_time = time.time()
+        print("Grab takes {:.2f} ms".format((that_time-now_time)*1E3))
+        # print(im0)
+        
+        return str(self.screen), im, im0, None, s
+        
