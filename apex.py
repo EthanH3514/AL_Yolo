ebd083b5b647e806196bd46a711c4594a94bbe11
EthanH3514 <1326414961@qq.com>
2023-06-20 15:47:39 +0800

    清理多余监听器、清理不必要的循环开支

--- apex.py
+++ apex.py
@@ -1,14 +1,9 @@
 # -*- coding: utf-8 -*-
 from detect import YOLOv5Detector
-import time
 import pynput
 import winsound
 import mouse_control
 
-
-capture_rate = 60
-interval = 1 / capture_rate
-start_time = time.time()
 
 detector = YOLOv5Detector(
     weights='best.pt',
@@ -23,7 +18,6 @@ def release(key):
     if key == pynput.keyboard.Key.home:  # Home
         winsound.Beep(400, 200)
         mouse_control.run()
-        return False
     elif key == pynput.keyboard.Key.end:  # End
         winsound.Beep(600, 200)
         mouse_control.stop()
@@ -34,16 +28,8 @@ if __name__ == '__main__':
     
     listener = pynput.keyboard.Listener(on_release=release)
     listener.start()
-    listener.join()
     
-    current_time = time.time()
-    elapsed_time = current_time - start_time
+    detector.work()
     
-    if elapsed_time >=interval:
-        start_time = time.time()
-        detector.work()
-        
-    listener = pynput.keyboard.Listener(on_release=release)
-    listener.start()
     listener.join()
 
